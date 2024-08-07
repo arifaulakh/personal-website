bc84e63b2afc428c8d252e9ebcd6381eca149139
arifaulakh <arifaulakh@gmail.com>
2024-08-07 03:00:03 -0400

    change head

--- app/head.tsx
+++ app/head.tsx
@@ -3,10 +3,10 @@ export default function Head() {
     <>
       <title>Arif Aulakh</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Arif Aulakh is currently an undergraduate student at the University of Toronto studying math, computer science and history." />
-      <meta name="keywords" content="Arif Aulakh, Arif Aulakh University of Toronto, Arif Aulakh UofT, Mathematics, Computer Science, Machine Learning"></meta>
+      <meta name="description" content="Arif Aulakh is a software engineer in NYC working at Check" />
+      <meta name="keywords" content="Arif Aulakh, Arif Aulakh University of Toronto, Arif Aulakh UofT, Check, Check Technologies, NYC, Mathematics, Computer Science, Machine Learning"></meta>
       <meta property="og:title" content="Arif Aulakh" />
-      <meta property="og:description" content="Arif Aulakh is currently an undergraduate student at the University of Toronto studying math, computer science and history." />
+      <meta property="og:description" content="Arif Aulakh is a software engineer in NYC working at Check." />
       <meta property="og:url" content="https://aaulakh.com" />
       <meta property="og:type" content="website" />
       <link rel="icon" href="/favicon.ico" />
