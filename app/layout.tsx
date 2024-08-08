8a833521c8558e8e7d8122d3856faaffd3a2e7aa
arifaulakh <arifaulakh@gmail.com>
2024-08-07 20:47:32 -0400

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,32 +12,33 @@ export default function RootLayout({
   const header = (
     <header>
       <div className="text-center my-6">
-        <Link href = "/">
+        <Link href="/">
           <h1 className="text-3xl text-black font-bold">
             Arif Aulakh
           </h1>
         </Link>
       </div>
     </header>
-  )
+  );
 
   const footer = (
     <footer>
-    <div className="mt-6 py-6 text-center text-slate-400">
-      <Socials></Socials>
-      <br></br>
-      <Link href ="/">
-      <h3 className="hover:underline">
-        Created by Arif Aulakh
-      </h3>
-      </Link>
-    </div>
-  </footer>
-  )
+      <div className="mt-6 py-6 text-center text-slate-400">
+        <Socials />
+        <br />
+        <Link href="/">
+          <h3 className="hover:underline">
+            Created by Arif Aulakh
+          </h3>
+        </Link>
+      </div>
+    </footer>
+  );
+
   return (
-      <html>
+    <html>
       <head />
-      <body className="dark: bg-gray-100">
+      <body className="dark:bg-gray-100">
         <div className="mx-auto max-w-2xl px-6">
           {header}
           {children}
@@ -46,6 +47,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-    
-  )
+  );
 }
