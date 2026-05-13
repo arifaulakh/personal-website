99aff2a3ca18ab12c627c5bbf32f344852ad1db7
arifaulakh <arifaulakh@gmail.com>
2026-05-13 16:18:01 -0700

    Update reading list

--- data/books.ts
+++ data/books.ts
@@ -9,6 +9,12 @@ export const books: Book[] = [
     {
         title: "John Adams",
         author: "David McCullough",
+        status: "completed",
+        year: 2026
+    },
+    {
+        title: "Technological Revolutions and Financial Capital: The Dynamics of Bubbles and Golden Ages",
+        author: "Carlota Perez",
         status: "reading",
         year: 2026
     }
